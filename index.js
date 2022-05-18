f0d1744078d33a42ed228b2984f66b7efe5f4fee
Vcsilva21 <victor30silva04@gmail.com>
2022-05-18 14:02:03 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const bandeiras = [
+  ['Brasil', ['Verde', 'Amarelo', 'Azul', 'Branco']],
+  ['Canadá',['Vermelho', 'Branco']],
+  ['México', ['Verde', 'Branco', 'Vermelho']]
+];
+
+console.log(bandeiras[1][1][1]);
